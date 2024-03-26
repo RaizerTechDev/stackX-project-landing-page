ae90a1830ed6cb2ea035eb98e94be48bf3cc647a
“RafaRz76Dev” <rafaelraizer76@gmail.com>
2024-03-26 20:36:31 -0300

    Commit Initial

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Rolagem suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
